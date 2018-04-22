5727fb798c790e454d4f5544f15cc2cc21edafa0
WittawatHamhongsa <pangpondsound@hotmail.com>
2018-04-22 16:22:16 +0700

    commit

---
+++ public/general/assets/js/MainAppCtrl.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var app = angular.module('app', [] );
+
+// app.config(function($stateProvider, $urlRouterProvider) {
+//   $urlRouterProvider.otherwise('/about');
+//
+//       $stateProvider
+//
+//       .state('about', {
+//         url: '/about',
+//         templateUrl: "http://localhost:2001/department_template/about.html",
+//         controller: 'aboutCtrl'
+//       })
+//       .state('enroll', {
+//         url: '/enroll',
+//         templateUrl: "http://localhost:2001/department_template/enroll.html",
+//         controller: 'enrollCtrl'
+//       })
+// });
+
+app.controller("mainCtrl", function($scope , $location) {
+  //write codes
+
+});
