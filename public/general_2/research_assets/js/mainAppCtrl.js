cd197f1860e2f9fd889ade55b864307f14c365e2
WittawatHamhongsa <pangpondsound@hotmail.com>
2018-05-01 16:56:27 +0700

    Pond compare overlap P'Mix

--- public/general_2/research_assets/js/mainAppCtrl.js
+++ public/general_2/research_assets/js/mainAppCtrl.js
@@ -88,7 +88,7 @@ angular.module('app')
 app.controller('homeCtrl', function($scope, $http,$state,$window) {
  console.log("homeCtrl start");
  //$window.scrollTo(0, 0); //up to top
- $state.go('profileDetail.general');
+ //$state.go('profileDetail.general');
  //console.log("paperDetail paperDetail");
 });
 app.controller('paperDetailCtrl', function($scope, $http) {
