245e9f39ce9909691a4bf8b2da95194e2a13587d
saktaya suksri <saktayasuksri@gmail.com>
2018-05-12 23:36:38 +0700

    lumkarn

--- public/general_2/research_assets/js/home.js
+++ public/general_2/research_assets/js/home.js
@@ -7,9 +7,17 @@ init();
 
 
  function init(){
+<<<<<<< HEAD
 	 $scope.search={};
 	 $scope.search.departmentName_TH='';
 
+=======
+     $scope.search={};
+     $scope.search.departmentData = {};
+	 $scope.search.departmentData.departmentName_TH='';
+     
+     $scope.loading = true;
+>>>>>>> parent of f95f432... ff
 	 $scope.researcher_list={};
 	  let dataObj = {
 
@@ -22,11 +30,29 @@ init();
 
             res.success(function(data, status, headers, config) {
                 //$scope.message = data;
+<<<<<<< HEAD
                 console.log("this getAllResearcherPreview = " +JSON.stringify(data))
                 $scope.researcher_list = data.data;
 
 
 
+=======
+                console.log("this getAllResearcherPreview_ai = " +JSON.stringify(data))
+                if(data.code != "999999")
+                {
+                        alert( JSON.stringify(data.code) )
+                }
+                else{
+
+                    
+                    $scope.researcher_list = data.data;
+                    $scope.loading = false;
+                }
+                
+              
+                
+             
+>>>>>>> parent of f95f432... ff
             });
             res.error(function(data, status, headers, config) {
                 alert( "failure message: " + JSON.stringify({data: data}) +"ไม่สามารถติดต่อเซิฟเวอร์ได้ ติดต่อแอดมิน");
@@ -36,7 +62,11 @@ init();
 
  $scope.filter_department = function(department_name){
 
+<<<<<<< HEAD
 	 $scope.search.departmentName_TH = department_name;
+=======
+	$scope.search.departmentData.departmentName_TH = department_name;
+>>>>>>> parent of f95f432... ff
 
  }
 
