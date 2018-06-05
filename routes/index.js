c6f05305aa90c601be6b53f2ca6530429de790fa
saktaya suksri <saktayasuksri@gmail.com>
2018-06-06 02:50:41 +0700

    UPDATE

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ var User = require('../model/user');
 
 
 
-
+const request = require('request');
+const querystring = require('querystring');
+const bot_gathering = require('./bot_gathering');
 
 module.exports = function (app, passport) {
 
@@ -15,6 +17,108 @@ module.exports = function (app, passport) {
         res.sendfile('public/general_2/index.html'); // load the index.ejs file
     });
 
+    function get_each_researcher(name_ary,leng){
+        
+                                let  count = leng;
+                                count--;
+                                console.log("------------ count  =  "+count)
+        
+                                if(count == -1)
+                                {
+                                    return null
+                                }
+                                else
+                                {
+        
+                                                  
+                        let str = name_ary[count].researcherName_EN;
+                        let buf_str = str.split(/\s+/g);
+        
+                        let firstname = buf_str[0];
+                        let lastname = buf_str[1];
+                                        if(firstname && lastname ){
+                    
+                                                bot_gathering.bot_gathering({
+                                                    _id:name_ary[count]._id,
+                                                    firstname:firstname,
+                                                    lastname:lastname
+                                
+                                                } ,function(){
+                                
+                                                    
+                                                    get_each_researcher(name_ary,count);
+                                            
+                                
+                                            });
+                    
+                                        }
+                                        else{
+                                            get_each_researcher(name_ary,count);
+
+                                        }
+                                }
+        
+                     
+               
+        
+                    
+        
+                      
+        
+                  
+        
+                      
+                 
+                    }
+
+    app.get('/bot', function (req, res) {
+        console.log(" /getAllResearcherName");
+        
+        
+    var form = {
+                }
+            var formData = querystring.stringify(form);
+            var contentLength = formData.length;
+            request({
+              header : {
+                'User-Agent':       'Super Agent/0.0.1',
+                'Content-Type':     'application/x-www-form-urlencoded'
+            },
+              uri: 'http://localhost:2000/api/getAllResearcherName/',
+              form: formData,
+              method: 'POST'
+            }, function (err, res2, body) {
+              //it works!
+              if(err){
+                  console.log("[ERROR] getAllResearcherName = "+err);
+                  res.send("error")
+              }
+              else{
+
+                
+               
+                body = JSON.parse(body)
+                if(body.code != '999999')
+                {
+                    res.send(JSON.stringify(body));
+
+                }
+                else{
+
+                    res.send("success")
+               
+                console.log('data in getAllResearcherName  = '+body)
+
+                get_each_researcher(body.data,body.data.length)
+         
+              }
+            }
+            });
+
+            
+    });
+
+    
      app.get('/research_centre', function (req, res) {
          console.log("template 2 ");
          res.sendfile('public/general/index.html'); // load the index.ejs file
